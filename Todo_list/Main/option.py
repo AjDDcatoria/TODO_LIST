bcbb38135f2bf0f7472fb356c86ac354ee49d0a5
AJ The Greate <108705731+AjProgrammer1@users.noreply.github.com>
2023-11-19 21:40:38 +0800

    Update option.py
    I'll move all the functions into class

--- Todo_list/Main/option.py
+++ Todo_list/Main/option.py
@@ -1,4 +1,4 @@
-"""
+""" #! IMPORTANT 
     To connect data base need to install 
     pip install mysql-connector-python  👈 this
 
@@ -6,13 +6,7 @@
 
     data base we used mysql (XAMPP)
 """
-
-from main import (
-    clear_screen,
-    clear_lines
-)
 import mysql.connector
-
 from datetime import datetime
 
 current_date = datetime.now().date()
@@ -34,155 +28,169 @@ class Connector: # DataBase Connector translated from java
         except mysql.connector.Error as err:
             print(f"Error: {err}")
 
-connector = Connector()
-connector.connect()
+class Options:
+    def __init__(self):
+        self.connector = Connector()
+        self.connector.connect()
 
-def add_task():
-    """
-    This block of code inserts values into the database.
+    def insert_task(self, task_name, description, deadline_str):
+        """
+        Inserts a task into the database.
 
-    Before inserting the deadlines if the user provide is 
-    already past ask if he/she wants to continue if yes
-    then insert the value into database
+        :param task_name: Title of the task
+        :param description: Description of the task
+        :param deadline_str: Deadline in MM-DD-YY format
+        :return: Message indicating the success or failure of the operation
 
-    Author: AJ R. Dedicatoria ☠️
-    Code created: 11-18-2023 to 11-19-2023 📅
-    """
+        Author : Aj R. Dedicatoria ☠️
+        Code created : 11-18-2023 📅
 
-    print('\t - Add Tasks -\n')
-    task_name = input('Enter Title       : ')
-    description = input('Enter Description : ')
-    deadline_str = input('Enter Deadline MM-DD-YY: ')
+        """
+        cursor = self.connector.con.cursor()
 
-    #! Check if the inputs are empty
-    if not task_name or not description or not deadline_str:
-        return 'Please input all the requirements'
+        # Execute SQL query to add a task
+        sql = "INSERT INTO todo_test (`title`, `description`, `deadlines`, `mark`) VALUES (%s, %s, %s, %s)"
+        values = (task_name, description, deadline_str, False)
 
-    cursor = None  # Initialize cursor to None
+        try:
+            cursor.execute(sql, values)
+            # Commit the changes to the database
+            self.connector.con.commit()
 
-    try:
-        parsed_date = datetime.strptime(deadline_str, "%m-%d-%Y")
+            return '\n\t  Task Added ✔️'
 
-        if parsed_date < datetime.now():
-            choice = input('The deadline you provided is already past.\nPlease enter "YES" or "NO" if you want to continue: ')
-            
-            # check if the user enter no then skip the insertion
-            if choice.upper() != 'YES':
-                return '\tTask not added. Deadline is past.'
-            else:
-                cursor = connector.con.cursor()
-                # Execute SQL query to add a task
-                sql = "INSERT INTO todo_test (`title`, `description`, `deadlines`, `mark`) VALUES (%s, %s, %s, %s)"
-                values = (task_name, description, parsed_date, False)
-                cursor.execute(sql, values)
+        except mysql.connector.Error as err:
+            return f"Error adding task: {err}"
 
-                # Commit the changes to the database
-                connector.con.commit()
-
-                return '\n\t  Task Added ✔️'
-
-    except ValueError:
-        return 'Invalid date format. Please use MM-DD-YY.'
-
-    except mysql.connector.Error as err:
-        return f"Error adding task: {err}"
-
-    finally:
-        # Close the cursor if it's not None
-        if cursor:
+        finally:
+            # Close the cursor
             cursor.close()
 
-def remove_task():
-    # TODO : Code to remove the task from the database 🔨
+    def add_task(self):
+        """
+        This block of code inserts values into the database.
+        Before inserting. check the deadlines if the user provides is 
+        already past, ask if they want to continue. If yes,
+        then insert the value into the database.
 
-    """ #? IDEA for building this function
+        Author: AJ R. Dedicatoria ☠️
+        Code created: 11-18-2023 to 11-19-2023 📅
+        """
 
-        You can use display_task() function to display the task
-        and delete by user input 
+        print('\t - Add Tasks -\n')
+        task_name = input('Enter Title       : ')
+        description = input('Enter Description : ')
+        deadline_str = input('Enter Deadline MM-DD-YY: ')
 
-        ikaw na bahala if gusto mo nan lahi na way pag display
-    
-    """
-    print('\t- Remove Tasks -\n')
-    return '\t  Task Remove ✔️'
+        # Check if the inputs are empty
+        if not task_name or not description or not deadline_str:
+            return 'Please input all the requirements'
 
+        try:
+            parsed_date = datetime.strptime(deadline_str, "%m-%d-%Y")
 
-def display_task():
-    # TODO : Code to display the task from the database 🔨
+            if parsed_date < datetime.now():
+                choice = input('The deadline you provided is already past.\nPlease enter "YES" or "NO" if you want to continue: ')
+                
+                # check if the user enters "no" then skip the insertion
+                if choice.upper() != 'YES':
+                    return '\tTask not added. Deadline is past.'
+                else:
+  
+                  return  self.insert_task(task_name,description,parsed_date)
 
-    """ #? IDEA for building this function
-                                            yyyy mm dd
-        The deadline value in data base ex. 2023-11-20
+            return self.insert_task(task_name,description,parsed_date)
 
-        before displaying the task calculate how many
-        days remaining before the deadlines. Sort them
-        by the remaining days ex. below
+        except ValueError:
+            return 'Invalid date format. Please use MM-DD-YY.'
 
-        display the task look like this
+        except mysql.connector.Error as err:
+            return f"Error adding task: {err}"
+
+    def remove_task(self):
+        # TODO: Code to remove the task from the database 🔨
+
+        """ #? IDEA for building this function
+            You can use display_task() function to display the task
+            and delete by user input
+            You can decide on the specific implementation details.
+        """
+        print('\t- Remove Tasks -\n')
+        return '\t  Task Remove ✔️'
+
+    def display_task(self):
+        # TODO: Code to display the task from the database 🔨
+
+        """ #? IDEA for building this function
+            The deadline value in the database is in the format ex. 2023-11-20
+            Before displaying the task, calculate how many
+            days remaining before the deadlines. Sort them
+            by the remaining days ex. below
+
+            Display the task as follows:
 
             ============ TODO LIST ============ 
 
-                1.Assignment#1 (TODO) 2 days left
-                2.Assignment#2 (TODO) 5 days left
-                3.Activity#1 (DONE) 9 days left
-        
-        addition the mark in data base is boolean 
-        0 = False == TODO
-        1 = True == DONE
+                1. Assignment#1 (TODO) 2 days left
+                2. Assignment#2 (TODO) 5 days left
+                3. Activity#1 (DONE) 9 days left
+        """
+        return 'displayTask'
 
-        Add feature print all TODO or Done task base user wants
-        before printing make sure to clear text in console using
+    def mark_task(self):
+        # TODO: Code to mark the task from the database / edit 🔨
 
-           ( clear_screen, ) = clear entire text
-           ( clear_lines ) = clear specific line bottom to top 
+        """ #? IDEA for building this function
+            The mark value in database is BOOLEAN
+            0 = False == TODO
+            1 = True == DONE
 
-           example in main.py file
-    """
+            You can use the display_task() function to display the task
+            and then ask the user for input on which task they want to mark.
+            You can decide on the specific implementation details.
+        """
+        return 'markTask'
 
-    return 'displayTask'
+    def update_task(self):
+        # TODO: Code to update or change the value of a task in the database 🔨
 
+        """ #? IDEA for building this function
+            Display the task and ask the user which task
+            they want to update.
 
-def mark_task():
-    # TODO :Code to mark the task from the database / edit 🔨
-
-    """ #? IDEA for building this function
-
-        the mark value in data base is BOOLEAN
-        0 = False == TODO
-        1 = True == DONE
-
-        You can use display_task() function to display the task
-        then ask the user input that he/she wants to mark
-
-        ikaw bahala kung gusto mo nan lahi na way pag display
-    """
-    return 'markTask'
-
-
-def update_task():
-    # TODO :Code to update or change the value of a task in the database 🔨
-
-    """ #? IDEA for building this function
-
-        display the task then ask user want the task
-        he/she wants to update
-
-        once the maka pili na siya nan task mag ask if uno iya otrohon
-        example
+            Once the user selects a task, ask them what they want to update.
+            For example:
 
             ============ TODO LIST ============
-                
-                1. Title
-                2. Description
-                3. Mark
-                4. DeadLine
+            
+            1. Title
+            2. Description
+            3. Mark
+            4. DeadLine
 
-            Enter your choise : // user input
-        
-        then uno iya piliOn amoy editon sa data base 
-    """
-    return 'updateTask'
+            Enter your choice: // user input
 
+            Then update the selected task in the database.
+        """
+        return 'updateTask'
 
-def default():
-    return 'Not in the options. Please try again!'
+    def default(self):
+        return 'Not in the options. Please try again!'
+
+    def get_all_tasks(self):
+        cursor = self.connector.con.cursor()
+
+        try:
+            sql = 'SELECT * FROM todo_test'
+            cursor.execute(sql)
+
+            result = cursor.fetchall()
+
+            task_list = [list(row) for row in zip(*result)]
+
+            return task_list
+
+        except mysql.connector.Error as err:
+            print(f"Error fetching tasks:{err}")
+        finally:
+            cursor.close()
